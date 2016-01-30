e4658647d6aca8d20bcb88c91aa36ebc99fa66bf
Andrew Heekin <andrewheekin@gmail.com>
2016-01-30 16:19:26 -0500

    functioning app

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
 // Declares the initial angular module "codermap". Module grabs other controllers and services.
-var app = angular.module('codermap', ['addCtrl', 'geolocation', 'gservice']);
+var app = angular.module('CoderMap', ['addCtrl', 'geolocation', 'gservice']);
