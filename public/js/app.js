b6056a9c3ae434e1bca686413a43e362a9ee9cfb
Andrew Heekin <andrewheekin@gmail.com>
2016-02-01 08:59:04 -0500

    query controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,18 @@
 // Declares the initial angular module "codermap". Module grabs other controllers and services.
-var app = angular.module('CoderMap', ['addCtrl', 'geolocation', 'gservice']);
+var app = angular.module('coderMap', ['addCtrl', 'queryCtrl', 'geolocation', 'gservice', 'ngRoute']);
+
+// Configures Angular routing -- showing the relevant view and controller when needed.
+app.config(function($routeProvider){
+  // Join Team Control Panel
+  $routeProvider.when('/join', {
+    controller: 'addCtrl', 
+    templateUrl: 'partials/addForm.html',
+
+  // Find Teammates Control Panel
+  }).when('/find', {
+  	controller: 'queryCtrl',
+    templateUrl: 'partials/queryForm.html',
+
+  // All else forward to the Join Team Control Panel
+  }).otherwise({redirectTo:'/join'})
+});
